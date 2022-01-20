881666316562cf1da17d61390f72c9fd4fbe6edd
Ayub <58354447+azzzub@users.noreply.github.com>
2022-01-20 21:42:06 +0700

    blynk

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,13 @@
 #include <HardwareSerial.h>
 #include "bq769x0.h" // Library for Texas Instruments bq76920 battery management IC
 
+#include <BlynkSimpleSerialBLE.h>
+
 #define BMS_ALERT_PIN PB_12 // attached to interrupt INT0
 #define BMS_BOOT_PIN PC_13  // connected to TS1 input
 #define BMS_I2C_ADDRESS 0x18
+
+char auth[] = "IFAhU9LxxWB6t5DuaYKIkX26-JGQ-1xV";
 
 #define LED PB_13
 
@@ -31,33 +35,22 @@ uint32_t columbCounter_mAs;
 float soc;
 float averageCellVoltage;
 int percent = -1;
-float nominalCapacity_Ah = 45;
+float nominalCapacity_Ah = 40;
 size_t numOcvPoints = sizeof(ocv_lfp) / sizeof(float);
 float *ocv = ocv_lfp;
 
 //*********
 
 void resetSOC(void);
+void sendSensor(void);
+
+BlynkTimer timer;
 
 void setup()
 {
-
   Serial.begin(19200);
   Serial3.begin(9600);
   pinMode(LED, OUTPUT);
-
-  // while (1)
-  // {
-  digitalWrite(LED, HIGH);
-  // delay(100);
-  // digitalWrite(LED, LOW);
-  // delay(100);
-  // digitalWrite(LED, HIGH);
-  // delay(100);
-  // digitalWrite(LED, LOW);
-  // delay(100);
-  // Serial.println("Test");
-  // }
 
   while (BMS.begin(BMS_ALERT_PIN, BMS_BOOT_PIN) == 1)
   {
@@ -91,11 +84,14 @@ void setup()
   BMS.setBalancingThresholds(0, 3450, 20); // minIdleTime_min, minCellV_mV, maxVoltageDiff_mV
   BMS.setIdleCurrentThreshold(300);
   BMS.enableAutoBalancing();
-  Serial.println("Cell_1,Cell_2,Cell_3,Cell_4,Cell_5,Total,Temp,Current");
   BMS.update();
   averageCellVoltage = (BMS.getBatteryVoltage() / 4.0) / 1000.0f;
   resetSOC();
   delay(1000);
+  Blynk.begin(Serial3, auth);
+
+  digitalWrite(LED, HIGH);
+  timer.setInterval(1000L, sendSensor);
 }
 
 bool loadStatus = true;
@@ -103,117 +99,43 @@ bool chargeStatus = true;
 
 void loop()
 {
-  if (Serial3.available())
-  {
-    String inputSerial = Serial3.readStringUntil(';');
-    if (inputSerial == "LD_0")
-    {
-      loadStatus = false;
-    }
-    else if (inputSerial == "LD_1")
-    {
-      loadStatus = true;
-    }
-    else if (inputSerial == "CG_1")
-    {
-      chargeStatus = true;
-    }
-    else if (inputSerial == "CG_0")
-    {
-      chargeStatus = false;
-    }
-  }
+  Blynk.run();
+  timer.run();
+}
 
-  if (loadStatus)
-  {
-    BMS.enableDischarging();
-  }
-  else
-  {
-    BMS.disableDischarging();
-  }
+WidgetLED led(V0);
 
-  if (chargeStatus)
-  {
-    BMS.enableCharging();
-  }
-  else
-  {
-    BMS.disableCharging();
-  }
+void sendSensor()
+{
+  digitalWrite(LED, LOW);
+  led.on();
+  delay(100);
+  digitalWrite(LED, HIGH);
+  led.off();
+  BMS.update();
+  BMS.enableDischarging();
+  BMS.enableCharging();
+  int cell1 = BMS.getCellVoltage(1);
+  int cell2 = BMS.getCellVoltage(2);
+  int cell3 = BMS.getCellVoltage(3);
+  int cell4 = BMS.getCellVoltage(5);
+  float totalBatteryVoltage = BMS.getBatteryVoltage() / 1000.0f;
+  float batteryCurrent = BMS.getBatteryCurrent() / 1000.0f;
+  columbCounter_mAs += batteryCurrent * 1000;
+  soc = columbCounter_mAs / (nominalCapacity_Ah * 3.6e4F);
+  float power = batteryCurrent * totalBatteryVoltage;
 
-  if (millis() - currentMillis >= 900)
-  {
-    digitalWrite(LED, LOW);
-    delay(100);
-    digitalWrite(LED, HIGH);
-    BMS.update();
-    BMS.enableDischarging();
-    BMS.enableCharging();
-
-    for (int i = 1; i <= 5; i++)
-    {
-      int cellVoltage = BMS.getCellVoltage(i);
-      if (cellVoltage <= 50)
-      {
-        continue;
-      }
-      if (currentDelta == 0)
-      {
-
-        currentDelta = cellVoltage;
-      }
-      else
-      {
-        delta += abs(currentDelta - cellVoltage);
-        currentDelta = cellVoltage;
-      }
-      cellVoltages[batteryCounter] = cellVoltage;
-      batteryCounter += 1;
-    }
-
-    Serial3.print(batteryCounter);
-    Serial3.print(",");
-
-    for (int i = 0; i < batteryCounter; i++)
-    {
-      Serial3.print(cellVoltages[i] / 1000.0f, 3);
-      Serial3.print(",");
-    }
-
-    batteryCounter = 0;
-
-    delta = delta / 4;
-    currentDelta = 0;
-
-    float totalBatteryVoltage = BMS.getBatteryVoltage() / 1000.0f;
-
-    Serial3.print(totalBatteryVoltage, 3);
-    Serial3.print(",");
-    float batteryCurrent = BMS.getBatteryCurrent() / 1000.0f;
-    columbCounter_mAs += batteryCurrent * 1000;
-    soc = columbCounter_mAs / (nominalCapacity_Ah * 3.6e4F);
-    Serial3.print(soc, 2);
-    Serial3.print("%,");
-    Serial3.print(delta / 1000.0f, 3);
-    Serial3.print(",");
-    Serial3.print(BMS.getMinCellVoltage() / 1000.0f, 3);
-    Serial3.print(",");
-    Serial3.print(BMS.getMaxCellVoltage() / 1000.0f, 3);
-    Serial3.print(",");
-    Serial3.print(BMS.getTemperatureDegC());
-    Serial3.print(",");
-
-    Serial3.print(abs(batteryCurrent), 2);
-    Serial3.print(",");
-    float power = abs(batteryCurrent * totalBatteryVoltage);
-    Serial3.print(power, 2);
-    Serial3.print(",");
-    Serial3.println(batteryCurrent > 0 ? 1 : batteryCurrent == 0 ? 0
-                                                                 : -1);
-
-    currentMillis = millis();
-  }
+  Blynk.virtualWrite(V1, String(cell1 / 1000.0f, 3));
+  Blynk.virtualWrite(V2, String(cell2 / 1000.0f, 3));
+  Blynk.virtualWrite(V3, String(cell3 / 1000.0f, 3));
+  Blynk.virtualWrite(V4, String(cell4 / 1000.0f, 3));
+  Blynk.virtualWrite(V5, String(totalBatteryVoltage, 3));
+  Blynk.virtualWrite(V6, String(batteryCurrent, 3));
+  Blynk.virtualWrite(V7, String(soc, 3));
+  Blynk.virtualWrite(V8, String(BMS.getMinCellVoltage() / 1000.0f, 3));
+  Blynk.virtualWrite(V9, String(BMS.getMaxCellVoltage() / 1000.0f, 3));
+  Blynk.virtualWrite(V10, String(BMS.getTemperatureDegC(), 2));
+  Blynk.virtualWrite(V11, String(power, 3));
 }
 
 void resetSOC()
